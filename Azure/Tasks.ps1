c77ce7a52434b067d86f9666846724be57dada58
Suri33 <sp@gmail.com>
2019-03-29 12:56:25 +0530

    Content Added

--- Azure/Tasks.ps1
+++ Azure/Tasks.ps1
@@ -1,6 +1,25 @@
 # Getting Storage Account Names belongs to Centralus
   # Get-AzStorageAccount | where location -eq 'centralus' | select Name 
 
-$items = Get-ChildItem -File -Recurse *
-$item = $items[0]
-echo $item.Name
+ # $items = Get-ChildItem -File -Recurse *
+ # $item = $items[0]
+ # echo $item.Name
+
+
+# Connect-AzAccount 
+
+  # find all the locations from Azure powershell
+   $locations  = Get-AzLocation
+
+   $currentlocation = $locations[2]
+   $resgrp = "ramrk"
+   $loc=  "central us"
+echo $currentlocation
+ # New-AzResourceGroup -Name $resgrp -Location $loc
+
+
+ New-AzResourceGroup -Name $resgrp -Location $loc
+
+
+ #  $resgrp 
+  # $loc
